7725edbb192f037b6640566b18346586c7e011c2
Firas M <firas@testing.com>
2024-11-25 23:51:01 +0300

    testing branch 1

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <>
       <div>
       <h1 className="text-3xl font-bold underline">
-      testing tailwindcss!
+      testing Firas's Branch!
     </h1>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
